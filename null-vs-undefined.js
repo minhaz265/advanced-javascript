b5566c6c5971614434bb60ebdba42fe8f6804377
minhaz265 <minhaz265@gmail.com>
2020-08-07 15:28:53 +0600

    null vs undefined all clear

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,31 +1,31 @@
-// let pakhi;
-// console.log(pakhi);
+let pakhi;
+console.log(pakhi);
 
 
 
-// function add(num1, num2){
-//     console.log(num1 + num2);
-// }
+function add(num1, num2){
+    console.log(num1 + num2);
+}
 
-// const result = add(13,82);
-// console.log(result);
+const result = add(13,82);
+console.log(result);
 
-// function printNum(num1, num2){
+function printNum(num1, num2){
 
-//     console.log(num1, num2);
-// } 
+    console.log(num1, num2);
+} 
 
-// const result2 = printNum(70);
-// console.log(result2);
-
-
-
-// const premik = {name:"smart dude", phone:04944}
-// console.log(premik.gf); 
+const result2 = printNum(70);
+console.log(result2);
 
 
-// let fun = undefined;
-// console.log(fun);
+
+const premik = {name:"smart dude", phone:04944}
+console.log(premik.gf); 
+
+
+let fun = undefined;
+console.log(fun);
 
 let ages = [2,5,9];
 console.log(ages[11]);
