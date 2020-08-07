2c153ca768391e154af81f9bbf573217c6772e8c
minhaz265 <minhaz265@gmail.com>
2020-08-07 15:45:24 +0600

    true-false.js

---
+++ true-false.js
@@ -0,0 +1,13 @@
+
+let name = null;
+console.log(name);
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// false 0 "" undefined null NaN false 
+// true digit " " empty array 
